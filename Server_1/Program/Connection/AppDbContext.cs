ce4101103899ff776957dfa4a453ef3edb413723
ordell <henrikekholm1987@protonmail.com>
2025-09-21 11:51:55 +0200

    worskapce created

---
+++ Server_1/Program/Connection/AppDbContext.cs
@@ -0,0 +1,29 @@
+using System.Security.Cryptography;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+public class AppDbContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer
+            ("Server=localhost,1433;Database=master;User Id=sa;Password=.nGzz8tqt9;TrustServerCertificate=True;");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Example of configuring an entity
+        // modelBuilder.Entity<User>(entity =>
+        // {
+        //     entity.HasKey(e => e.Id);
+        //     entity.Property(e => e.Username).IsRequired().HasMaxLength(50);
+        //     entity.Property(e => e.Email).IsRequired().HasMaxLength(100);
+        // });
+
+        /*public DbSet<User> Users { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }*/
+    }
+}
