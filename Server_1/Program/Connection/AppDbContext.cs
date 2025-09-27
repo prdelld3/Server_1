309b5250392b6628da1cd699cfa1e7da2d26ee6a
ordell <henrikekholm1987@protonmail.com>
2025-09-27 12:37:01 +0200

    credits

--- Server_1/Program/Connection/AppDbContext.cs
+++ Server_1/Program/Connection/AppDbContext.cs
@@ -5,8 +5,8 @@ using Microsoft.Extensions.Options;
 public class AppDbContext : DbContext
 {
     public DbSet<Author> Authors { get; set; }
-
     public DbSet<Book> Books { get; set; }
+    public DbSet<Credit> Credits { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
@@ -15,9 +15,29 @@ public class AppDbContext : DbContext
     }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<Book>()
+         .HasIndex(b => b.Title)
+            .IsUnique();
+
         modelBuilder.Entity<Author>()
-        .HasIndex(a => a.Name)
-        .IsUnique();
+            .HasIndex(a => a.Name)
+            .IsUnique();
+
+        modelBuilder.Entity<Credit>()
+            .HasIndex(c => new { c.BookId, c.AuthorId })
+            .IsUnique();
+
+        modelBuilder.Entity<Book>()
+            .HasMany(b => b.Credits)
+            .WithOne(c => c.Book)
+            .HasForeignKey(c => c.BookId)
+            .IsRequired();
+
+        modelBuilder.Entity<Author>()
+            .HasMany(a => a.Credits)
+            .WithOne(c => c.Author)
+            .HasForeignKey(c => c.AuthorId)
+            .IsRequired();
     }
 }
 
@@ -25,6 +45,7 @@ public class Author
 {
     public int AuthorId { get; set; }
     public string? Name { get; set; }
+    public List<Credit> Credits { get; set; } = new();
 
 }
 
@@ -32,5 +53,14 @@ public class Book
 {
     public int BookId { get; set; }
     public string? Title { get; set; }
-    //public List<Author> Credits { get; set; } = new();
+    public List<Credit> Credits { get; set; } = new();
+}
+
+public class Credit
+{
+    public int CreditId { get; set; }
+    public int BookId { get; set; }
+    public Book? Book { get; set; }
+    public int AuthorId { get; set; }
+    public Author? Author { get; set; }
 }
